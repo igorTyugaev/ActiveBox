88c8fa71b1448a6e3569bbec21c10401b6c37ea9
godzzadsdlla <igor.tyugaev@gmail.com>
2020-02-23 16:34:13 +0500

    add nav menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+
+  // Fixed Header
   let header = $("#header");
   let intro = $("#intro");
   let introH = intro.innerHeight();
@@ -15,6 +17,29 @@ $(function () {
     }
   });
 
+  // Nav Toggle
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+    nav.toggleClass("show");
 
+  });
+
+  // Smooth scroll
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementID = $(this).data("scroll");
+    let elementOffset = $(elementID).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate({
+      scrollTop: elementOffset - 70
+    }, 700)
+
+    console.log(elementOffset);
+  });
 
 });
